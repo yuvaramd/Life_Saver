0f0bc399eb3e80e284f2761f73b1a465a7bea8de
abhay8046 <abhaydere8046@gmail.com>
2022-12-05 05:07:25 -0600

    Request accepted implementation

--- src/pages/hospitalHome/HospitalHome.jsx
+++ src/pages/hospitalHome/HospitalHome.jsx
@@ -5,9 +5,7 @@ import ReqForm from "../../components/reqForm/ReqForm";
 function HospitalHome() {
     return (
         <div className="hh-container" >
-            <div className="hh-left">
-                More information & Feature coming soon!
-            </div>
+            
             <div className="hh-right">
                 <ReqForm/>
             </div>

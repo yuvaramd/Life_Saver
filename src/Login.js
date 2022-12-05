022c3e6fbb24328062e138aca80f88f8ed9a4ee1
abhay8046 <65994687+abhay8046@users.noreply.github.com>
2022-12-05 05:46:37 -0600

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -5,8 +5,19 @@ import { auth } from "./firebase";
 
 function Login() {
     const history = useHistory();
+    
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [hospitalType, setHospitalType] = useState(false);
+
+    const checkboxControl = (e) => {
+        e.preventDefault()
+        setHospitalType(e.target.checked)
+    }
+    
+    const linkToPush = hospitalType ? '/hospitalinfo' : '/userinfo'
+    const linkToPushForHome = hospitalType ? '/hospitalhome' : '/userhome'
+    
 
     const signIn = e => {
         e.preventDefault();
@@ -14,7 +25,7 @@ function Login() {
         auth
             .signInWithEmailAndPassword(email, password)
             .then(auth => {
-                history.push('/')
+                history.push(linkToPushForHome)
             })
             .catch(error => alert(error.message))
     }
@@ -28,7 +39,7 @@ function Login() {
                 console.log(auth);
                 // it successfully created a new user with email and password
                 if (auth) {
-                    history.push('/')
+                    history.push(linkToPush)
                 }
             })
             .catch(error => alert(error.message))
@@ -39,12 +50,12 @@ function Login() {
             <Link to ="/">
                 <img  
                     className = "login__logo"
-                    src = "https://i.ibb.co/fxqSs8h/finallogo2.png" 
+                     
                 />
             </Link>
 
             <div className="login__container">
-                <h1>Sign-in</h1>
+                <h1>Sign-in / Register</h1>
                 <form>
                     <h5>E-mail</h5>
                     <input type="text" value = {email} onChange = 
@@ -53,16 +64,19 @@ function Login() {
                     <h5>Password</h5>
                     <input type = "password" value = {password} onChange = 
                     {e => setPassword(e.target.value)}/>
-
+                    <div className='checkbox-ui'>
+                        <h5>Login/Register as Hospital</h5>
+                        <input type="checkbox" value={hospitalType} onChange={checkboxControl}></input>
+                    </div>
+                  
                     <button type="submit" onClick={signIn}
                     className="login__signInButton">Sign in</button>
+                    
                 </form>
-                <p>
-                    By continuing, you agree to Life Stream's Conditions of Use and Privacy Notice.
-                </p>    
+                 
 
                 <button onClick = {register}
-                className="login__registerButton">Create Life Stream Account</button>
+                className="login__registerButton">Create Firestream Account</button>
             </div>
         </div>
     )
